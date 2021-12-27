f3c540734b21d8b3e1126faf19015e9b8e53e6b1
unknown <yueshuaipeng@cecbrain.com>
2021-12-27 15:52:13 +0800

    .

---
+++ src/main/java/lordy/pointers/exercise/LongestWordInDictionaryThroughDeleting.java
@@ -0,0 +1,15 @@
+package lordy.pointers.exercise;
+
+import java.util.List;
+
+/**
+ * No.524
+ */
+//TODO
+public class LongestWordInDictionaryThroughDeleting {
+
+    public static String findLongestWord(String s, List<String> dictionary){
+
+        return null;
+    }
+}
--- src/main/java/lordy/pointers/exercise/ValidPalindrome.java
+++ src/main/java/lordy/pointers/exercise/ValidPalindrome.java
@@ -3,15 +3,35 @@ package lordy.pointers.exercise;
 /**
  * No.680
  */
-//TODO
 public class ValidPalindrome {
 
     public static boolean validPalindrome(String s){
-        return false;
+        int i = 0, j = s.length() - 1;
+        while (i < j){
+            if(s.charAt(i) != s.charAt(j)){
+                return isValid(s, i + 1, j) || isValid(s, i, j - 1);
+            }
+            i++;
+            j--;
+        }
+        return true;
     }
+
+    public static boolean isValid(String s, int i, int j){
+        while (i < j){
+            if(s.charAt(i) != s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
 
     public static void main(String[] args) {
         String s = "aba";
         System.out.println(validPalindrome(s));
     }
+
 }
