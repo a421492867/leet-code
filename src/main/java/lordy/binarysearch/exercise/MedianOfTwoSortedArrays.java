fa84fa6d8d3b4dce7bf2c6e72c23614c6fddba77
unknown <yueshuaipeng@cecbrain.com>
2021-12-28 17:27:23 +0800

    .

---
+++ src/main/java/lordy/binarysearch/exercise/MedianOfTwoSortedArrays.java
@@ -0,0 +1,13 @@
+package lordy.binarysearch.exercise;
+
+/**
+ * No.4
+ */
+//TODO
+public class MedianOfTwoSortedArrays {
+
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2){
+
+        return 0L;
+    }
+}
--- src/main/java/lordy/binarysearch/exercise/SingleElementInSortedArray.java
+++ src/main/java/lordy/binarysearch/exercise/SingleElementInSortedArray.java
@@ -3,11 +3,33 @@ package lordy.binarysearch.exercise;
 /**
  * No.540
  */
-//TODO
 public class SingleElementInSortedArray {
 
     public static int singleNonDuplicate(int[] nums){
+        int l = 0, r = nums.length - 1, mid;
+        while (l < r){
+            mid = l + (r - l) / 2;
+            if(nums[mid] == nums[mid - 1]){
+                if((mid - l) % 2 == 0){
+                    r = mid - 2;
+                }else {
+                    l = mid + 1;
+                }
+            }else if (nums[mid] == nums[mid + 1]){
+                if((r - mid) % 2 == 0){
+                    l = mid + 2;
+                }else {
+                    r = mid - 1;
+                }
+            }else {
+                return nums[mid];
+            }
+        }
+        return nums[l];
+    }
 
-        return 0;
+    public static void main(String[] args) {
+        int nums[] = {3,3,7,7,10,11,11};
+        System.out.println(singleNonDuplicate(nums));
     }
 }
