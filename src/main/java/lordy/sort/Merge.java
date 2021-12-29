0980eda6253bfc221f346e546e8eaa24abd6c9a9
unknown <yueshuaipeng@cecbrain.com>
2021-12-29 14:30:32 +0800

    .

--- src/main/java/lordy/sort/Merge.java
+++ src/main/java/lordy/sort/Merge.java
@@ -2,7 +2,44 @@ package lordy.sort;
 
 public class Merge {
 
-    public static void sort(Comparable[] nums){
+    private static Comparable[] aux;
 
+    public static void sort(Comparable[] nums){
+        aux = new Comparable[nums.length];
+        sort(nums, 0, nums.length - 1);
+    }
+
+    public static void sort(Comparable[] nums, int l, int r){
+        if(r <= l) return;
+        int mid = l + (r - l) / 2;
+        sort(nums, l, mid);
+        sort(nums, mid + 1, r);
+        m(nums, l, mid, r);
+    }
+
+    public static void m(Comparable[] nums, int l, int mid, int r){
+        int i = l, j = mid + 1;
+        for(int k = l; k <= r; k++){
+            aux[k] = nums[k];
+        }
+        for(int k = l; k <= r; k++){
+            if(i > mid){
+                nums[k] = aux[j++];
+            }else if(j > r){
+                nums[k] = aux[i++];
+            }else if(aux[j].compareTo(aux[i]) < 0){
+                nums[k] = aux[j++];
+            }else {
+                nums[k] = aux[i++];
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Integer[] nums = {2,5,8,3,5,0,7,19,4};
+        sort(nums);
+        for(int s : nums){
+            System.out.println(s);
+        }
     }
 }
