1bf9118a52d47bf359875d2dd34236bcd012e532
unknown <yueshuaipeng@cecbrain.com>
2021-12-31 16:58:37 +0800

    .

--- src/main/java/lordy/search/Permutations.java
+++ src/main/java/lordy/search/Permutations.java
@@ -1,13 +1,39 @@
 package lordy.search;
 
+import com.alibaba.fastjson.JSON;
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * No.46
  */
+//TODO
 public class Permutations {
 
     public static List<List<Integer>> permute(int[] nums){
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        backtrack(res, list, nums);
+        return res;
+    }
 
+    public static void backtrack(List<List<Integer>> res, List<Integer> list, int[] nums){
+        if(list.size() == nums.length){
+            res.add(new ArrayList<>(list));
+            return;
+        }
+        for(int num : nums){
+            if(!list.contains(num)){
+                list.add(num);
+                backtrack(res, list, nums);
+                list.remove(list.size() - 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3};
+        System.out.println(JSON.toJSON(permute(nums)));
     }
 }
