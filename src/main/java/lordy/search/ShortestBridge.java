30021c4c5b0553a8b278bc169a7d7c5e46debf53
unknown <yueshuaipeng@cecbrain.com>
2022-01-05 18:21:08 +0800

    .

---
+++ src/main/java/lordy/search/ShortestBridge.java
@@ -0,0 +1,20 @@
+package lordy.search;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * No.934
+ */
+public class ShortestBridge {
+
+    public static int bridge(int[][] grid){
+        return 0;
+    }
+
+
+    public static void main(String[] args) {
+        int[][] grid = {{0, 1, 0}, {0,0,0}, {0,0,1}};
+        bridge(grid);
+    }
+}
