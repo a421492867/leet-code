3f702bfa86bb9b5107c8231bf0bcd406bd57154c
unknown <yueshuaipeng@cecbrain.com>
2022-03-08 17:43:09 +0800

    .

---
+++ src/main/java/lordy/hot100/Leetcode33.java
@@ -0,0 +1,32 @@
+package lordy.hot100;
+
+public class Leetcode33 {
+
+    public int search(int[] nums, int target){
+
+        int n = nums.length;
+        if(n == 0) return -1;
+        if(n == 1){
+            return nums[0] == target ? 0 : -1;
+        }
+        int l = 0, r = n - 1;
+        while (l <= r){
+            int mid = l + (r - l) / 2;
+            if(nums[mid] == target) return mid;
+            if(nums[l] <= nums[mid]){
+                if(target >= nums[l] && target < nums[mid]){
+                    r = mid - 1;
+                }else {
+                    l = mid + 1;
+                }
+            }else {
+                if(target <= nums[r] && target > nums[mid]){
+                    l = mid + 1;
+                }else {
+                    r = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
