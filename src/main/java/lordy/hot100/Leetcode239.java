9d5b03a91b63600f9a8a763d904320acb428ab9f
unknown <yueshuaipeng@cecbrain.com>
2022-03-10 12:41:21 +0800

    .

--- src/main/java/lordy/hot100/Leetcode239.java
+++ src/main/java/lordy/hot100/Leetcode239.java
@@ -1,27 +1,52 @@
 package lordy.hot100;
 
-/**
- * 超出时间限制
- */
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+
 public class Leetcode239 {
 
     public int[] maxSlidingWindow(int[] nums, int k){
         int n = nums.length;
+        Queue<Integer> queue = new PriorityQueue<Integer>(((o1, o2) -> nums[o2] - nums[o1]));
         int[] res = new int[n - k + 1];
-        int pre = 0, cur = k;
-        while (cur <= n){
-            findMax(res, nums, pre, cur);
-            pre++;
-            cur++;
+        for(int i = 0; i < n; i++){
+            queue.offer(i);
+            if (!queue.isEmpty()){
+                while (queue.peek() <= i - k){
+                    queue.poll();
+                }
+            }
+            if(i >= k - 1){
+                res[i - k + 1] = nums[queue.peek()];
+            }
         }
         return res;
     }
 
-    public void findMax(int[] res, int[] nums, int pre, int end){
-        int max = Integer.MIN_VALUE;
-        for(int i = pre; i < end; i++){
-            max = Math.max(nums[i], max);
-        }
-        res[pre] = max;
+    public static void main(String[] args) {
+        Leetcode239 l = new Leetcode239();
+        int[] nums = {1,3,-1,-3,5,3,6,7};
+        l.maxSlidingWindow(nums, 3);
     }
+
+//    public int[] maxSlidingWindow(int[] nums, int k){
+//        int n = nums.length;
+//        int[] res = new int[n - k + 1];
+//        int pre = 0, cur = k;
+//        while (cur <= n){
+//            findMax(res, nums, pre, cur);
+//            pre++;
+//            cur++;
+//        }
+//        return res;
+//    }
+//
+//    public void findMax(int[] res, int[] nums, int pre, int end){
+//        int max = Integer.MIN_VALUE;
+//        for(int i = pre; i < end; i++){
+//            max = Math.max(nums[i], max);
+//        }
+//        res[pre] = max;
+//    }
 }
