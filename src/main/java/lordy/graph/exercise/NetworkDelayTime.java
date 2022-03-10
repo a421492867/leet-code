c41225c933333db4643ab98afafe9c8c5bc2be36
unknown <yueshuaipeng@cecbrain.com>
2022-03-10 15:15:56 +0800

    .

--- src/main/java/lordy/graph/exercise/NetworkDelayTime.java
+++ src/main/java/lordy/graph/exercise/NetworkDelayTime.java
@@ -1,20 +1,26 @@
 package lordy.graph.exercise;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * No.743
+ */
 public class NetworkDelayTime {
 
     public static int networkDelayTime(int[][] times, int n, int k){
 
 
         return 0;
+
+
     }
 
     public static void main(String[] args) {
         int[][] times = {{2,1,1},{2,3,1},{3,4,1}};
         int n = 4;
         int k = 2;
-        networkDelayTime(times, n, k);
+        System.out.println(networkDelayTime(times, n, k));
     }
 }
