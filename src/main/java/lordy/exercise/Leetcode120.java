1e91d584099d837220833b13f2018da26a43dd68
unknown <yueshuaipeng@cecbrain.com>
2022-04-28 11:32:22 +0800

    .

---
+++ src/main/java/lordy/exercise/Leetcode120.java
@@ -0,0 +1,32 @@
+package lordy.exercise;
+
+import java.util.List;
+
+//TODO
+public class Leetcode120 {
+
+    public int minimumTotal(List<List<Integer>> triangle){
+        int n = triangle.size();
+
+        int[][] dp = new int[n][n];
+
+        dp[0][0] = triangle.get(0).get(0);
+
+        for(int i = 1; i < n; i++){
+            dp[i][0] = dp[i - 1][0] + triangle.get(i).get(0);
+            for(int j = 1; j < i; j++){
+                dp[i][j] = Math.min(dp[i - 1][j - 1], dp[i - 1][j]) + triangle.get(i).get(j);
+            }
+            dp[i][i] = dp[i - 1][i - 1] + triangle.get(i).get(i);
+        }
+
+        int min = dp[n - 1][0];
+
+        for(int i = 1; i < n; i++){
+            min = Math.min(min, dp[n - 1][i]);
+        }
+        return min;
+    }
+
+
+}
