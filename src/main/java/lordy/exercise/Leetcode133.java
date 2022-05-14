83b8bb322e18cecd86c9cc869fc5daf48f5bb6ae
unknown <yueshuaipeng@cecbrain.com>
2022-05-14 14:40:37 +0800

    .

---
+++ src/main/java/lordy/exercise/Leetcode133.java
@@ -0,0 +1,42 @@
+package lordy.exercise;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Leetcode133 {
+
+    class Node{
+        public int val;
+        public List<Node> neighbors;
+
+        public Node() {
+            val = 0;
+            neighbors = new ArrayList<Node>();
+        }
+
+        public Node(int val, List<Node> neighbors) {
+            this.val = val;
+            this.neighbors = neighbors;
+        }
+    }
+
+    private Map<Node, Node> visited = new HashMap<>();
+    public Node cloneGraph(Node node){
+
+        if(node == null) return node;
+
+        if(visited.containsKey(node)){
+            return visited.get(node);
+        }
+
+        Node cloneNode = new Node(node.val, new ArrayList<>());
+        visited.put(node, cloneNode);
+        for(Node neighbor : node.neighbors){
+            cloneNode.neighbors.add(cloneGraph(neighbor));
+        }
+        return cloneNode;
+
+    }
+}
