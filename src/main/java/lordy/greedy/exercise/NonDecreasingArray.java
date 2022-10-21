1971aa852ca6f5e0349d9af0b13c851d49219e55
yueshuaipeng <yueshuaipeng@cecbrain.com>
2022-10-21 14:32:48 +0800

    665

--- src/main/java/lordy/greedy/exercise/NonDecreasingArray.java
+++ src/main/java/lordy/greedy/exercise/NonDecreasingArray.java
@@ -7,7 +7,24 @@ package lordy.greedy.exercise;
 public class NonDecreasingArray {
 
     public boolean checkPossibility(int[] nums){
+        int n = nums.length;
+        int[] array = new int[n + 2];
+        array[0] = Integer.MIN_VALUE;
+        array[n + 1] = Integer.MAX_VALUE;
+        System.arraycopy(nums, 0, array, 1, nums.length);
 
-        return false;
+        int res = 0;
+
+        for(int i = 1; i < n + 2; i++){
+            if(array[i] < array[i - 1] && array[i] >= array[i - 2]){
+                res++;
+                array[i - 1] = array[i];
+            }else if(array[i] < array[i - 1] && array[i] < array[i - 2]){
+                array[i] = array[i - 1];
+                res++;
+            }
+        }
+
+        return res <= 1;
     }
 }
