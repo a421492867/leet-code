853aeeccaef7fbf2e2d2ec40c1f241ce525507e3
yueshuaipeng <yueshuaipeng@cecbrain.com>
2022-11-22 14:08:09 +0800

    .

---
+++ src/main/java/lordy/codetop/bytedance/Leetcode124.java
@@ -0,0 +1,23 @@
+package lordy.codetop.bytedance;
+
+import lordy.tree.TreeNode;
+
+public class Leetcode124 {
+
+    int res = Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root){
+        helper(root);
+        return res;
+    }
+
+    private int helper(TreeNode node){
+        if(node == null) return 0;
+        int lVal = helper(node.left);
+        int rVal = helper(node.right);
+        res = Math.max(lVal + node.val, res);
+        res = Math.max(rVal + node.val, res);
+        res = Math.max(lVal + node.val + rVal, res);
+        res = Math.max(node.val, res);
+        return Math.max(node.val, Math.max(lVal, rVal) + node.val);
+    }
+}
