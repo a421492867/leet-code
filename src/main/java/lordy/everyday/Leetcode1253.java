a5df447ef37d6b5c16217f371254dc96f6e61379
a421492867 <speng.y@foxmail.com>
2023-06-29 09:37:48 +0800

    .

---
+++ src/main/java/lordy/everyday/Leetcode1253.java
@@ -0,0 +1,47 @@
+package lordy.everyday;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Leetcode1253 {
+
+    public List<List<Integer>> reconstructMatrix(int upper, int lower, int[] colsum) {
+        int n = colsum.length;
+        int sum = 0, two = 0;
+        for (int i = 0; i < n; ++i) {
+            if (colsum[i] == 2) {
+                ++two;
+            }
+            sum += colsum[i];
+        }
+        if (sum != upper + lower || Math.min(upper, lower) < two) {
+            return new ArrayList<List<Integer>>();
+        }
+        upper -= two;
+        lower -= two;
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        for (int i = 0; i < 2; ++i) {
+            res.add(new ArrayList<Integer>());
+        }
+        for (int i = 0; i < n; ++i) {
+            if (colsum[i] == 2) {
+                res.get(0).add(1);
+                res.get(1).add(1);
+            } else if (colsum[i] == 1) {
+                if (upper > 0) {
+                    res.get(0).add(1);
+                    res.get(1).add(0);
+                    --upper;
+                } else {
+                    res.get(0).add(0);
+                    res.get(1).add(1);
+                }
+            } else {
+                res.get(0).add(0);
+                res.get(1).add(0);
+            }
+        }
+        return res;
+
+    }
+}
