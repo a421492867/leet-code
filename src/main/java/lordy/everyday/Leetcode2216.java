2c3f662d0479c0acb55cdb548c7549ee72d994fa
a421492867 <speng.y@foxmail.com>
2023-11-23 18:28:27 +0800

    .

---
+++ src/main/java/lordy/everyday/Leetcode2216.java
@@ -0,0 +1,18 @@
+package lordy.everyday;
+
+public class Leetcode2216 {
+
+    public int minDeletion(int[] nums) {
+        int n = nums.length;
+        int ans = 0;
+        for(int i = 0; i < n - 1; i++){
+            if(nums[i] == nums[i + 1]){
+                ans++;
+            }else {
+                i++;
+            }
+        }
+        ans += (n - ans) % 2;
+        return ans;
+    }
+}
